5d31b8e0323b048e3c298793bc889daf1a5c4d6d
buudee625 <liu.andrew625@gmail.com>
2022-07-22 00:45:07 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+const WORDS = [
+  "DEVELOPER",
+  "ENGINEER",
+  "NODE",
+  "JAVASCRIPT",
+  "CODING",
+  "HTML",
+  "GUI",
+  "BOOLEAN",
+  "REACT",
+  "FUNCTION",
+  "COMPUTER SCIENCE",
+  "SEPARATION OF CONCERNS",
+];
+const WRONG_GUESS_HUNG_COUNT = 6;
+const SPRITE_WIDTH = 15;
+
+
